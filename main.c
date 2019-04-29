c21058561e9dada964c3d2e1b7177af21536890e
Gabe Hennessy-O'Reilly <gabe.hennessyoreilly@uon.edu.au>
2019-04-29 02:26:36 +0000

    Pretty well functional

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 //This allows for the absolute value function to be used
 #include <stdlib.h>
 
+//This allows for manipulation of strings without manual characterwise modifications
 #include <string.h>
 
 //This will shift the alphabet array by the encryption key (numerical)
@@ -61,6 +62,10 @@ void caesarCipher(void) {
     //This is the encrypted message, read from message.txt
     FILE *input;
     input = fopen("\message.txt", "r");
+    if(ferror(input)) {
+        printf("Error opening file");
+        return;
+    }
     
     /*This determines the length of the file by adding 1 every time the file pointer
     position is incremented until the end of file*/
@@ -116,8 +121,18 @@ void caesarCipher(void) {
         //This will encrypt the message
         tranMessage(code, codeLength, alpArr);
 
-        //Print the encrypted message
-        printf("%s \n", code);
+        //Open the file for writing
+        FILE *output;
+        output = fopen("\output.txt");
+        
+        //If it cannot open, abort
+        if(ferror(output)) {
+                printf("Error opening file");
+                return;
+        }
+        //Otherwise print the message
+        fprintf(output, "%s", code);
+        fclose(output);
          
     } else { //Otherwise
         //Allows the choice to decrypt with or without a key
@@ -232,7 +247,7 @@ void unknownCaesar(char *toDec, int toDecLength, char *alpArr) {
         alpArrCopy[alpInd] = alpArr[alpInd];
     }
 
-    //This resests back to the original message for multiple attempts
+    //This resets back to the original message for multiple attempts
     for(int ind = 0; ind < toDecLength; ind++) {
         toDecCopy[ind] = toDec[ind];
     }
@@ -247,8 +262,10 @@ void unknownCaesar(char *toDec, int toDecLength, char *alpArr) {
         arrayShiftDe(alpArr, count);
         tranMessage(toDecCopy, toDecLength, alpArr);
         
-        //If the word 'THE' is found, print the solution
-        if(wordsCorrect(toDecCopy, toDecLength) >= 0.5) {
+        //If 2 of the first 5 words are english
+        if(wordsCorrect(toDecCopy, toDecLength) >= 0.4) {
+            
+            //Assume the message is correct
             fprintf(output, "%s", toDecCopy);
             fclose(output);
             return;
@@ -295,16 +312,27 @@ void subCipher(void) {
     while(!feof(input)) {
         //Add another letter from the message file into the message string
         message[tempInd1] = fgetc(input);
+        if(message[tempInd1] < 123 && message[tempInd1] > 96)
+            message[tempInd1] -= 32;
         //Increment this index to enable the next character to be added
         tempInd1++;
     }
     
     //Provides the option of encryption and decryption
-    printf("Type 0 to encrypt and 1 to decrypt");
+    printf("Type 0 to encrypt and 1 to decrypt: ");
     scanf("%d", &encOrDec);
+    
+    //If encrypting
     if(!encOrDec) {
         //Opens the key for encryption and decryption
-        FILE *key = fopen("\subKey.txt", "r");
+        FILE *key;
+        key = fopen("\subKey.txt", "r");
+        
+        //If there is an error opening the file, abort
+        if(ferror(key)) {
+            printf("Error opening file");
+            return;
+        }
         
         //While the key file still contains more letters
         while(!feof(key)) {
@@ -325,13 +353,18 @@ void subCipher(void) {
             
         }
         //Print the message and close the file
-        FILE *output = fopen("\output.txt", "w");
+        FILE *output;
+        output= fopen("\output.txt", "w");
+        if(ferror(output)) {
+            printf("Error opening file");
+            return;
+        }
         fprintf(output, "%s", message);
         fclose(output);
     } else {
         //Stores whether a key is needed
         int keyReq;
-        printf("Type 0 to decrypt without a key or 1 to decrypt with one");
+        printf("Type 0 to decrypt without a key or 1 to decrypt with one: ");
         scanf("%d", &keyReq);
         if(keyReq) {
             //Opens the key for encryption and decryption
@@ -357,16 +390,87 @@ void subCipher(void) {
                 }
             }
             
-            //Print the message and close the file
-            FILE *output = fopen("\output.txt", "w");
+            //Open the file, print the message and close the file
+            FILE *output;
+            output = fopen("\output.txt", "w");
+            if(ferror(output)) {
+                printf("Error in opening file");
+                return;
+            }
             fprintf(output, "%s", message);
             fclose(output);
 
+        } else {//If a key is required
+            //Stores the amount of each letter encountered
+            char letterCount[26];
+            
+            //Keeps a copy of the original message to make comparison and modification much simpler (avoids errors when reading and modifying)
+            char messageCopy[messageLength];
+            //Copies the original message
+            strcpy(messageCopy, message);
+            
+            //Determines the amount of letters that show up more frequently than any given letter
+            char moreFrequent[26];
+            
+            //Initialises all elements of these arrays to 0 for incrementation
+            for(int i = 0; i < 26; i++) {
+                letterCount[i] = 0;
+                moreFrequent[i] = 0;
+            }
+            
+            
+            //Whenever a letter is encountered, increment how often that letter has appeared
+            for(int i = 0; i < messageLength; i++) {
+                if(message[i] < 91 && message[i] > 64)
+                    letterCount[message[i]%65]++;
+            }
+            
+            //These are ordered from the most frequent to the least frequent for common english use
+            char letterOcc[] = "ETAOINSRHDLUCMFYWGPBVKXQJZ";
+            
+            //Cycles through and compares the frequency with which letters appear in the message
+            for(int ind = 0; ind < 26; ind++) {
+                for(int i = ind+1; i < 26; i++) {
+                    
+                    //If one letter is more frequent than another, increment the amount of letters which appear more frequently than the other one
+                    if(letterCount[ind] > letterCount[i])
+                        moreFrequent[i]++;
+                    else //Otherwise increment the amount of letters more frequent than the first letter
+                        moreFrequent[ind]++;
+                }
+                
+            }
+
+            //Cycle through the message
+            for(int i = 0; i < messageLength; i++) {
+                for(int ind = 0; ind < 26; ind++) {
+                    //If the character is a capital letter
+                    if(message[i] == ind + 65) {
+                        //Convert this letter to the letter in the same frequency position in the letterOcc string (i.e. normal english)
+                        messageCopy[i] = letterOcc[moreFrequent[ind]];
+                        //Quit the for loop to avoid modifying this character again
+                        break;
+                    }
+                }
+                
+            }
+            //Opens the file for writing
+            FILE *output;
+            output = fopen("\output.txt", "w");
+            
+            //If it cannot open, quit
+            if(ferror(output)) {
+                printf("Error in opening file");
+                return;
+            }
+            
+            //Otherwise print the message
+            fprintf(output, "%s", messageCopy);
+            fclose(output);
         }
+        
     }
 
-    
-    
 }
 
 
@@ -402,11 +506,11 @@ float wordsCorrect(char *message, int messageLength) {
     //Stores the amount of words to be tested to verify if decryption has worked
     int wordsToTest;
     
-    //Set this to be spaces + 1 with an upper bound of 5 words
-    if(spaces < 5)
-        wordsToTest = spaces + 1;
+    //Set this to be spaces + 1 with an upper bound of 4 words
+    if(spaces < 4)
+        wordsToTest = spaces + 4;
     else
-        wordsToTest = 5;
+        wordsToTest = 4;
         
     //Stores the first 5 words with a maximum length of 30 characters each
     char currWord[wordsToTest][30];
@@ -416,18 +520,24 @@ float wordsCorrect(char *message, int messageLength) {
     
     //Cycle through the words
     for(int i = 0; i < wordsToTest; i++) {
-        if(i == 0) //For the first word, set its length to the amount of characters before the first space
+        if(i == 0) {//For the first word, set its length to the amount of characters before the first space
             wordLength = spacePos[0];
-        else //For the rest, set their lengths to the distance from one space to the next
+        } else {//For the rest, set their lengths to the distance from one space to the next
             wordLength = spacePos[i] - spacePos[i-1] - 1;
-        
+        }
+    
+
+
         //Takes the message word by word
         for(int currLet = 0; currLet < wordLength; currLet++) {
             //If it is not the first word, take the space position as a reference point and store the letters
-            if(i != 0)
+            if(i != 0) {
                 currWord[i][currLet] = message[spacePos[i-1] + currLet + 1];
-            else //If it is the first word, store the letters
+            } else {//If it is the first word, store the letters
                 currWord[i][currLet] = message[currLet];
+                
+            }
+
         }
         
         //If currWord[i] is a word, add it to the wordcount
